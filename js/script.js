b43fd691a1c0391aaa3d3daa7f30619a87d18f0c
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-07-02 13:28:21 +0200

    added new function onToggleButtonClick which has now bodyElement inside

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 {
-    const bodyElement = document.body;
-
-    const toggleBackground = () => {
+    const toggleBackground = (bodyElement) => {
         bodyElement.classList.toggle("js-darkMode");
     }
 
-    const toggleButtonInnerText = () => {
+    const toggleButtonInnerText = (bodyElement) => {
         document.querySelector(".js-themeName").innerText = bodyElement.classList.contains("js-darkMode") ? "DARK" : "LIGHT";
+    }
+
+    const onToggleButtonClick = () => {
+        const bodyElement = document.body;
+
+        toggleButtonInnerText(bodyElement);
+        toggleBackground(bodyElement);
     }
 
     const init = () => {
         const toggleBackgroundButton = document.querySelector(".js-toggleBackgroundButton");
 
-        toggleBackgroundButton.addEventListener("click", () => {
-            toggleButtonInnerText();
-            toggleBackground();
-
-        });
+        toggleBackgroundButton.addEventListener("click", onToggleButtonClick);
     }
 
     init()
