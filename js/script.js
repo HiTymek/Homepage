766a47e6caf4f10ee7acb8d53b3b85df1baa640b
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-07-02 13:13:27 +0200

    delete free spaces in script.js file

--- js/script.js
+++ js/script.js
@@ -2,19 +2,14 @@
     const bodyElement = document.body;
 
     const toggleBackground = () => {
-
         bodyElement.classList.toggle("js-darkMode");
-
     }
 
     const toggleButtonInnerText = () => {
-
         document.querySelector(".js-themeName").innerText = bodyElement.classList.contains("js-darkMode") ? "DARK" : "LIGHT";
-
     }
 
     const init = () => {
-
         const toggleBackgroundButton = document.querySelector(".js-toggleBackgroundButton");
 
         toggleBackgroundButton.addEventListener("click", () => {
@@ -22,7 +17,6 @@
             toggleBackground();
 
         });
-
     }
 
     init()
